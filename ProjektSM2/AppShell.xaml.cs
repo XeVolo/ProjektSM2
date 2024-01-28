c123e426c772cd3ac1c7a07ecfe350971cd34f79
xevolo <xevolo12@gmail.com>
2024-01-28 20:00:52 +0100

    Dodaj pliki projektów.

---
+++ ProjektSM2/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ProjektSM2.ViewModels;
+using ProjektSM2.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ProjektSM2
+{
+	public partial class AppShell : Xamarin.Forms.Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+			Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+			Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+		}
+
+		private async void OnMenuItemClicked(object sender, EventArgs e)
+		{
+			await Shell.Current.GoToAsync("//LoginPage");
+		}
+	}
+}
---
+++ ProjektSM2/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ProjektSM2.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ProjektSM2.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
