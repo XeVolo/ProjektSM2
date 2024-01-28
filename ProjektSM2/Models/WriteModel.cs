e754b91878b16e19d30097dfbf1ef530c2430be4
Mixus <mateuszokragly@gmail.com>
2024-01-28 23:13:59 +0100

    baza dziala

--- ProjektSM2/Models/WriteModel.cs
+++ ProjektSM2/Models/WriteModel.cs
@@ -5,11 +5,11 @@ namespace ProjektSM2.Models
     public class WriteModel
 	{
 		[PrimaryKey, AutoIncrement]
-		public int id { get; set; }
-		public string question { get; set; }
-		public string answer { get; set; }
-		public string incorrectans1 { get; set; }
-		public string incorrectans2 { get; set; }
-		public string incorrectans3 { get; set; }
+		public int Id { get; set; }
+		public string Question { get; set; }
+		public string Answer { get; set; }
+		public string Incorrectans1 { get; set; }
+		public string Incorrectans2 { get; set; }
+		public string Incorrectans3 { get; set; }
 	}
 }
--- ProjektSM2/Views/NewQuestionPage.xaml.cs
+++ ProjektSM2/Views/NewQuestionPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ProjektSM2.Models;
 
 namespace ProjektSM2.Views
 {
@@ -16,5 +17,23 @@ namespace ProjektSM2.Views
 		{
 			InitializeComponent();
 		}
-	}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(questionEntry.Text))
+            {
+                await App.Database.SaveWriteAsync(new WriteModel
+                {
+                    Question = questionEntry.Text,
+                    Answer = ansEntry.Text,
+                    Incorrectans1 = inAns1Entry.Text,
+                    Incorrectans2 = inAns2Entry.Text,
+                    Incorrectans3 = inAns3Entry.Text
+                });
+                questionEntry.Text = ansEntry.Text = inAns1Entry.Text = inAns2Entry.Text = inAns3Entry.Text = string.Empty;
+
+
+            }
+        }
+    }
 }
