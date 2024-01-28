509f1b492dcba644dc6d2bb38db7cf8ffddcfe00
Mixus <mateuszokragly@gmail.com>
2024-01-28 22:48:41 +0100

    Odpowiedzi

--- ProjektSM2/Views/ChoosePage.xaml.cs
+++ ProjektSM2/Views/ChoosePage.xaml.cs
@@ -1,21 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ProjektSM2.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ChoosePage : ContentPage
 	{
 		public ChoosePage()
 		{
 			InitializeComponent();
+		}
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+            collectionView.ItemsSource = await App.Database.GetWritesAsync();
 		}
 		private void AnsButtonClick(object sender, EventArgs e)
 		{
