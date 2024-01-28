2e47e48b37c102366e9eac981c19f1f09908110e
xevolo <xevolo12@gmail.com>
2024-01-28 22:06:00 +0100

    model

---
+++ ProjektSM2/Models/WriteModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjektSM2.Models
+{
+	public class WriteModel
+	{
+		public int id { get; set; }
+		public string question { get; set; }
+		public string answer { get; set; }
+		public string incorrectans1 { get; set; }
+		public string incorrectans2 { get; set; }
+		public string incorrectans3 { get; set; }
+	}
+}
