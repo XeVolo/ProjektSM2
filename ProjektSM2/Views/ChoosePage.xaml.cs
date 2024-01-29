81a47ac54b46dd8d5c934eb59987971997788310
Mixus <mateuszokragly@gmail.com>
2024-01-29 23:01:45 +0100

    zawiesza sie

--- ProjektSM2/Views/ChoosePage.xaml.cs
+++ ProjektSM2/Views/ChoosePage.xaml.cs
@@ -1,36 +1,78 @@
 ﻿using ProjektSM2.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ProjektSM2.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ChoosePage : ContentPage
-	{
-		List<WriteModel> items = new List<WriteModel>();
-		public ChoosePage()
-		{
-			InitializeComponent();
-			
-		}
-		protected override async void OnAppearing()
-		{
-			base.OnAppearing();
+    public partial class ChoosePage : ContentPage
+    {
+        List<WriteModel> items = new List<WriteModel>();
+        private int index = 0;
+        public ChoosePage()
+        {
+            InitializeComponent();
+
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
             items = await App.Database.GetWritesAsync();
 
-			Question.Text = items.First().Question;
-			Button1.Text = items.First().Answer;
-		}
-		private void AnsButtonClick(object sender, EventArgs e)
-		{
-			if (sender is Button clickedButton)
-			{
+            NewSet();
+        }
+
+        private void AnsButtonClick(object sender, EventArgs e)
+        {
+            if (sender is Button clickedButton)
+            {
 
 
-			}
-		}
-	}
+            }
+        }
+        private int GetRandom()
+        {
+            return new Random().Next(0, items.Count);
+        }
+
+        private List<int> GetNumbers()
+        {
+            int count = 4;
+            Random rnd = new Random();
+            List<int> numbers = new List<int>();
+
+            while (numbers.Count < count)
+            {
+                int liczba = rnd.Next(0, 3);
+                if (!numbers.Contains(liczba))
+                {
+                    numbers.Add(liczba);
+                }
+            }
+
+            return numbers;
+        }
+        private void NewSet()
+        {
+            index = GetRandom();
+            WriteModel currentWrite = items[index];
+
+            Question.Text = currentWrite.Question;
+
+            List<string> choices = new List<string>();
+            index = GetRandom();
+            choices.Add(items[index].Answer);
+            choices.Add(items[index].Incorrectans1);
+            choices.Add(items[index].Incorrectans2);
+            choices.Add(items[index].Incorrectans3);
+            List<int> drawn = GetNumbers();
+
+            Button1.Text = choices[drawn[0]];
+            Button2.Text = choices[drawn[1]];
+            Button3.Text = choices[drawn[2]];
+            Button4.Text = choices[drawn[3]];
+        }
+    }
 }
