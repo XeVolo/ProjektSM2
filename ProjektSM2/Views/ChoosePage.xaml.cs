110f29eacf33d403463496c9e83fd01448d79156
xevolo <xevolo12@gmail.com>
2024-01-29 22:23:50 +0100

    choos naprawione

--- ProjektSM2/Views/ChoosePage.xaml.cs
+++ ProjektSM2/Views/ChoosePage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
-
+﻿using ProjektSM2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,20 +10,25 @@ namespace ProjektSM2.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ChoosePage : ContentPage
 	{
+		List<WriteModel> items = new List<WriteModel>();
 		public ChoosePage()
 		{
 			InitializeComponent();
+			
 		}
 		protected override async void OnAppearing()
 		{
 			base.OnAppearing();
-            collectionView.ItemsSource = await App.Database.GetWritesAsync();
+            items = await App.Database.GetWritesAsync();
+
+			Question.Text = items.First().Question;
+			Button1.Text = items.First().Answer;
 		}
 		private void AnsButtonClick(object sender, EventArgs e)
 		{
 			if (sender is Button clickedButton)
 			{
-			
+
 
 			}
 		}
