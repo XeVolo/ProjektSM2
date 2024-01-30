9b9cf55dcfe67da223b286c86e9452ca3d103ae4
xevolo <xevolo12@gmail.com>
2024-01-30 18:21:26 +0100

    lokalizacja

--- ProjektSM2/Views/AboutPage.xaml.cs
+++ ProjektSM2/Views/AboutPage.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using Plugin.Geolocator;
+using System;
 using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,9 +12,11 @@ namespace ProjektSM2.Views
 {
 	public partial class AboutPage : ContentPage
 	{
+		private const string GoogleApiKey = "AIzaSyC6oWZj9kYtylKNcQkGAehpj2VveJx8GIA";
 		public AboutPage()
 		{
 			InitializeComponent();
+			GetLocation();
 		}
 		private async void OnNavigateToWritePageClicked(object sender, EventArgs e)
 		{
@@ -30,6 +37,43 @@ namespace ProjektSM2.Views
 		{
 
 			await Navigation.PushAsync(new ItemsPage());
+		}
+		private async void GetLocation()
+		{
+			var locator = CrossGeolocator.Current;
+			locator.DesiredAccuracy = 50; // Ustaw dokładność lokalizacji (w metrach)
+
+			try
+			{
+				var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+
+				
+				string address = await GetAddressFromCoordinates(position.Latitude, position.Longitude);
+
+				LabelLocation.Text = $"Szerokość geo.: {position.Latitude},\n Długość geo.: {position.Longitude}\nAdres: {address}";
+			}
+			catch (Exception ex)
+			{
+				
+				LabelLocation.Text = $"Unable to get location: {ex.Message}";
+			}
+		}
+		private async Task<string> GetAddressFromCoordinates(double latitude, double longitude)
+		{
+			using (var client = new HttpClient())
+			{
+				string apiUrl = $"https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key={GoogleApiKey}";
+
+				var response = await client.GetStringAsync(apiUrl);
+
+				// Analiza odpowiedzi JSON
+				var jsonObject = JObject.Parse(response);
+				var results = jsonObject["results"];
+				var firstResult = results?.FirstOrDefault();
+				var formattedAddress = firstResult?["formatted_address"]?.ToString();
+
+				return formattedAddress;
+			}
 		}
 	}
 }
